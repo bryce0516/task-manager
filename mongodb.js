1cc401b1368296ce816201422181ed99ebda69e1
Hyesung Park <51251258+bryce0516@users.noreply.github.com>
2021-06-14 17:13:37 +0900

    install mongoose

---
+++ mongodb.js
@@ -0,0 +1,52 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+const id = new ObjectID();
+
+console.log(id, id.getTimestamp());
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Andrew",
+    //     age: 27,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unalbe to insert user");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Gunther",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unalbe to insert documents");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+  }
+);
