ad49d4b0b55d4e659a956062a55b4ad1f425ce16
bryce0516 <jayhspark@gmail.com>
2021-06-23 15:34:40 +0900

    finish jwt

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,7 +9,8 @@ router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -21,8 +22,9 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    const token = user.generateAuthToken();
-    res.send(user);
+    const token = await user.generateAuthToken();
+    console.log("this is jwt token", token);
+    res.send({ user, token: token });
   } catch (e) {
     console.log(e);
     res.status(400).send(e);
