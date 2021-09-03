68216e708a3ea8974ac301ae76cc8f94d427664c
Hyesung Park <jayhspark@gmail.com>
2021-09-03 21:52:03 +0900

    finish update

--- src/routers/task.js
+++ src/routers/task.js
@@ -31,17 +31,26 @@ router.post("/tasks", auth,  async (req, res) => {
   }
 });
 
-router.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((tasks) => {
-      res.send(tasks);
-    })
-    .catch((error) => {
-      console.log("task get has error", error);
-      res.status(500).send(error);
-    });
-});
+// router.get("/tasks", (req, res) => {
+//   Task.find({})
+//     .then((tasks) => {
+//       res.send(tasks);
+//     })
+//     .catch((error) => {
+//       console.log("task get has error", error);
+//       res.status(500).send(error);
+//     });
+// });
 
+router.get("/tasks", auth, async (req, res) => {
+  const _id = req.params.id;
+  try{
+    await req.user.populate('tasks').execPopulate()
+    res.send(req.user.tasks)
+  }catch(error) {
+    res.status(500).send(error)
+  }
+})
 // router.get("/tasks/:id", auth, (req, res) => {
 //   const _id = req.params.id;
 //   Task.findById(_id)
@@ -57,19 +66,20 @@ router.get("/tasks", (req, res) => {
 // });
 
 router.get("/tasks/:id", auth,  async (req, res) => {
+  const _id = req.params.id
   try {
     const task = await Task.findOne({_id, owner: req.user._id})
-
     if(!task) {
       return res.status(404).send()
     }
+
     res.status(200).send(task)
   }catch(error) {
     res.status(500).send()
   } 
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every((update) =>
@@ -81,9 +91,9 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
-    updates.forEach((update) => (task[update] = req.body[update]));
-    await task.save();
+    const task = await Task.findOne({_id:req.params.id, owner:req.user._id});
+    // const task = await Task.findById(req.params.id);
+
 
     // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
     //   new: true,
@@ -92,6 +102,9 @@ router.patch("/tasks/:id", async (req, res) => {
     if (!task) {
       return res.status(404).send();
     }
+    updates.forEach((update) => (task[update] = req.body[update]));
+    await task.save();
+    
     res.send(task);
   } catch (error) {
     res.status(400).send(error);
