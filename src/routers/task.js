bb5eaa208a88e2e5b85916e45267243f36a78ca6
Hyesung Park <jayhspark@gmail.com>
2021-12-02 22:00:54 +0900

    update

--- src/routers/task.js
+++ src/routers/task.js
@@ -45,12 +45,17 @@ router.post("/tasks", auth,  async (req, res) => {
 
 //get /tasks?completed={true | false}
 //get /tasks?limit={number}&skip={number}
+//get /tasks?sortBy={createdAt : {desc = -1| aesc = 1}}
 router.get("/tasks", auth, async (req, res) => {
 
   const match = {}
-
+  const sort = {}
   if(req.query.completed) {
-    match.completed = req.query.completed === "true" ? "true": "false"
+    match.completed = req.query.completed === "true"
+  }
+  if(req.query.sortBy) {
+    const parts = req.query.sortBy.split(":")
+    sort[parts[0]] = parts[1] === 'desc' ? -1: 1
   }
 
   try{
@@ -59,7 +64,10 @@ router.get("/tasks", auth, async (req, res) => {
       match,
       options:{
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort: {
+          createdAt: parseInt(req.query.sortBy)
+        }
       }
     }).execPopulate()
     res.send(req.user.tasks)
